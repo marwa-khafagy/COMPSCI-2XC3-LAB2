d2e322c8361bb72e51588064f3b2c099e24aec99
Alex Eckardt <realalexeck@gmail.com>
2023-02-09 13:46:17 -0500

    Placed BFS2, DFS2 and DFS3 into graph.py file

--- source/DFS2.py
+++ source/DFS2.py
@@ -1,27 +1,6 @@
 from graph import *
 
 
-def DFS2Helper(G, node, node2, visited, path):
-    if node == node2:
-        path.append(node)
-        return path
-    path.append(node)
-    visited[node] = True
-    for adj_node in G.adj[node]:
-        if not visited[adj_node]:
-            return DFS2Helper(G, adj_node, node2, visited, path)
-    return []
-
-
-def DFS2(G, node1, node2):
-    visited = {}
-    path = []
-
-    for node in G.adj:
-        visited[node] = False
-
-    return DFS2Helper(G, node1, node2, visited, path)
-
 
 g = Graph(6)
 g.add_edge(0, 1)
