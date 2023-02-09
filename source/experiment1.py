14586f7fa8c5839de08ac0a337211f55d109e974
Alex Eckardt <realalexeck@gmail.com>
2023-02-09 14:01:46 -0500

    Removed duplicate bfs definition in experiment1

--- source/experiment1.py
+++ source/experiment1.py
@@ -1,26 +1,6 @@
-from graph import Graph
+from graph import *
 from collections import deque
 import random
-
-#Breadth First Search (TA added the comments as hints)
-def BFS(G, node1, node2):
-    Q = deque([node1])
-    marked = {node1 : True}
-    for node in G.adj:
-        if node != node1:
-            marked[node] = False
-    while len(Q) != 0:
-        current_node = Q.popleft()
-        for node in G.adj[current_node]:
-            if node == node2: #reached our destination
-                #return path
-                #should we return here or not
-                return True
-            if not marked[node]: 
-                Q.append(node)
-                #add node to path
-                marked[node] = True
-    return False
 
 #TA made this function
 def create_random_graph(i,j):
